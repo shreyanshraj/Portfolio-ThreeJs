058f18e03d42376dc36a59bbe3593e7166f5c99d
shreyanshraj <shreyanshraj@gmail.com>
2025-10-07 17:29:01 -0700

    ambient lighting and placeholder project

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+
 // Scene Setup
 const scene = new THREE.Scene();
 const canvas = document.getElementById('experience-canvas');
@@ -10,18 +13,176 @@ const sizes = {
     height: window.innerHeight
 };
 
+let intersectObject = "";
+
 const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
 renderer.setSize( sizes.width, sizes.height );
 renderer.setPixelRatio( Math.min(window.devicePixelRatio, 2) );
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;  
+renderer.shadowMap.enabled = true;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+// renderer.toneMappingExposure = 1.5;
 
+
+// console.log(THREE.REVISION);
+
+
+const intersectObjectsNames = [
+    "Cube006_Baked",
+    "Cube017_Baked",
+    "Cube003_Baked",
+    "Cube002_Baked",
+    "mattress_Baked",
+    "window_Baked",
+    "lower_window_Baked",
+    "shade_Baked",
+    "window_shade1_Baked",
+    "window_shade2_Baked",
+    "floor_Baked",
+    "wall_Baked",
+    "ceiling_Baked",
+    "desk_top",
+    "left_left_top",
+    "left_leg_bottom",
+    "left_left_supp",
+    "right_left_top",
+    "right_left_bottom",
+    "right_left_supp",
+    "system_Baked",
+    "system_top_Baked",
+    "Plane079_Baked",
+    "Plane080_Baked",
+    "Plane081_Baked",
+    "Plane082_Baked",
+    "Cube007_Baked",
+    "Plane101",
+    "Plane101_1",
+    "Plane060_Baked",
+    "Plane062_Baked",
+    "Plane064_Baked",
+    "Plane066_Baked",
+    "Plane059_Baked",
+    "Plane061_Baked",
+    "Plane063_Baked",
+    "Plane065_Baked",
+    "Plane067_Baked",
+    "Text003_Baked",
+    "Text002_Baked",
+    "Text004_Baked",
+    "Text001_Baked",
+    "Carpet_Baked",
+    "clock_Baked",
+    "fl_base_Baked",
+    "fl_base_up_Baked",
+    "fl_lamp_stand_Baked",
+    "fl_lamp_arm_Baked",
+    "lamp_Baked",
+    "fl_lamp_hang_Baked",
+    "fl_arm_conn_Baked",
+    "Plane071_Baked",
+    "wall_light_conn_Baked",
+    "wall_light_Baked",
+    "wl_left1_Baked",
+    "wl_left2_Baked",
+    "wl_3_Baked",
+    "wl_4_Baked",
+    "Plane057_Baked",
+    "House_Plant_Dracaena_Lemon_Lime_Baked",
+    "Plane_A1_Plant_A_0001_Baked",
+    "Plane_A1001_Plant_A_0001_Baked",
+    "Plane_A1002_Plant_A_0001_Baked",
+    "Plane_A1003_Plant_A_0001_Baked",
+    "Plane_A1001_Plant_A_0002_Baked",
+    "Plane058_Baked",
+    "Plane072_Baked",
+    "Plane073_Baked",
+    "Plane074_Baked",
+    "Plane075_Baked",
+    "Plane076_Baked",
+    "Plane077_Baked",
+    "Plane078_Baked",
+    "medal_3_Baked",
+    "medal_4_Baked",
+    "medal_hm_Baked",
+    "social_media_shelf_supp001_Baked",
+    "Chair_Chair_0_Baked",
+    "Cylinder001_Baked",
+    "Chair_Chair_0001_Baked",
+    "Plane052_Baked",
+    "Plane053_Baked",
+    "Plane054_Baked",
+    "Plane055_Baked",
+    "Plane056_Baked",
+    "f1_shelf_supp_Baked",
+    "social_media_shelf_supp_Baked",
+    "mail_logo_Baked",
+    "github_logo_Baked",
+    "linkedin_Baked",
+    "Plane001_Baked",
+    "Plane002_Baked",
+    "Plane003_Baked",
+    "Plane005_Baked",
+    "Object_2_Baked",
+    "monitor_base_l_Baked",
+    "monitor_base_r_Baked",
+    "keyboard_Baked",
+    "mouse_Baked",
+    "pen_stand_Baked",
+    "mug_Baked",
+    "pen_Baked",
+    "Legal_Note_Pad_White_Baked",
+    "base_Baked",
+    "lamp_conn_Baked",
+    "lamp_top_Baked",
+    "monitor_base_r001",
+    "wallpaper_r001",
+    "monitor_base_l001",
+    "wallpaper_l001",
+    "Cylinder007_Baked",
+    "Cylinder008_Baked",
+    "Cylinder009_Baked",
+    "Cylinder012_Baked",
+    "Cylinder013_Baked",
+    "Cylinder014_Baked",
+    "BézierCurve003_Baked",
+    "BézierCurve004_Baked",
+    "BézierCurve005_Baked",
+    "backstand_l_Baked",
+    "screen_l_Baked",
+    "back_stand_R_Baked",
+    "screen_r_Baked",
+    "bulb_Baked",
+    "legs001_Baked",
+    "legs_Baked",
+    "plafon_Baked",
+    "screen_r_Baked",
+    "bulb_Baked",
+    "legs001_Baked",
+    "legs_Baked",
+    "plafon_Baked"
+];
+
+const intersectObjects = [];
 
 // Loadng Blender Model
 const loader = new GLTFLoader();
 
 loader.load( './rc_final_export.glb', function ( glb ) {
     // console.log( glb.scene.children[0].name );
-    
-  scene.add( glb.scene );
+
+    glb.scene.traverse( ( child ) => {
+        if(intersectObjectsNames.includes(child.name)){
+            intersectObjects.push(child);
+        }
+        if ( child.isMesh ) {
+            child.castShadow = true;
+            child.receiveShadow = true;
+            // console.log(child.name);
+            // child.material.metalness = 0;
+        }
+    } );
+
+    scene.add( glb.scene );
 
 }, undefined, function ( error ) {
 
@@ -29,6 +190,28 @@ loader.load( './rc_final_export.glb', function ( glb ) {
 
 } );
 
+
+const sun = new THREE.DirectionalLight( 0xfbc08d);
+sun.castShadow = true;  
+sun.position.set( 150, 180, 0 );
+sun.intensity = 1;
+sun.shadow.camera.left = -4;
+sun.shadow.camera.right = 4;
+sun.shadow.camera.top = 4;
+sun.shadow.camera.bottom = -4;
+sun.shadow.normalBias = 0.05;
+
+scene.add( sun );
+
+// console.log(sun.position);
+
+
+const shadowHelper = new THREE.CameraHelper( sun.shadow.camera );
+scene.add( shadowHelper );
+
+
+const helper = new THREE.DirectionalLightHelper( sun, 5 );
+scene.add( helper );
 
 // ambient Light
 const light = new THREE.AmbientLight( 0x404040, 3 );
@@ -45,7 +228,7 @@ const camera = new THREE.OrthographicCamera(
     1, 
     1000 );
 
-camera.position.x = 7.151;
+camera.position.x = 6.151;
 camera.position.y = 5.536;
 camera.position.z = 7.462;
 
@@ -74,13 +257,43 @@ function handleResize() {
     
 }
 
+function handlePointerMove( event ) {
+	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
+
+
 window.addEventListener('resize', handleResize);
+window.addEventListener('pointermove', handlePointerMove);
+window.addEventListener('click', handleClick);
+
+function handleClick() {
+    console.log(intersectObject);
+    
+};
 
 // Animation Loop
 function animate() {    
-    console.log('====================================');
-    console.log(camera.position);
-    console.log('====================================');
+    // console.log('====================================');
+    // console.log(camera.position);
+    // console.log('====================================');
+    raycaster.setFromCamera( pointer, camera );
+
+	// calculate objects intersecting the picking ray
+	const intersects = raycaster.intersectObjects( intersectObjects );
+
+    if (intersects.length > 0) {
+        document.body.style.cursor = 'pointer';
+    } else {
+        document.body.style.cursor = 'default';
+        intersectObject= "";
+    }   
+
+	for ( let i = 0; i < intersects.length; i ++ ) {
+        // console.log(intersects[0].object.name);
+        intersectObject=  intersects[0].object.name;
+
+	}
 
     renderer.render( scene, camera );
 
